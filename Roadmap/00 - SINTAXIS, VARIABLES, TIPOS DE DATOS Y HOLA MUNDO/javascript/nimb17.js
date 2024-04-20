ac8f7e0b3c0fef5a259316762b20e62e4252d2cf
Hector-M <hola@nicobm.com>
2024-04-19 22:06:24 -0400

    Reto 0

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/nimb17.js
@@ -0,0 +1,41 @@
+/*  
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+
+
+// ------------------------ RESPUESTA ------------------------ 
+
+
+// 1.- https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// 2.- Una linea
+
+/* 3.- Dos 
+       Lineas */
+
+let ecmaScript = 0;
+const yo = 1;
+
+let number = 0;
+let string = "string";
+let booleanos = true;
+let undefined = undefined;
+let nul = null
+
+console.log("¡Hola, JavaScript!");
+
+
+
+
+
